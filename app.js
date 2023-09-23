f099d6a38448106d5c9e9c714577f733d3dac1d7
Shahrukh <shahrukh1494@gmail.com>
2023-09-23 17:51:55 +0530

    Server side HTML with templates, EJS, REST routes

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const locationRoutes = require("./routes/location");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Type", "text/html");
-  next();
-});
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
-app.use((req, res, next) => {
-  res.send("<h1>Hello World!</h1>");
-});
+app.use(bodyParser.json());
+
+app.use(locationRoutes);
+
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   const userName = req.body.username || 'Unknown User';
+//   res.render('index', {
+//     user: userName
+//   });
+// });
 
 app.listen(3000);
